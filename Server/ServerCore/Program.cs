4322a3b224434909fb89e9e30a5b5ea2fcf2cfa9
Yijun <gkqls813@gamil.com>
2023-02-02 15:40:39 +0900

    Implement simple socket communication

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace ServerCore
 {
@@ -6,7 +9,46 @@ namespace ServerCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // DNS
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[1];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7000);
+
+            // Tcp 소켓 생성
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // 소켓 바인드
+                listenSocket.Bind(endPoint);
+                // TODO : backlog
+                listenSocket.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Listening...");
+
+                    Socket clientSocket = listenSocket.Accept();
+
+                    // Receive
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = clientSocket.Receive(recvBuff);
+                    string recvData = Encoding.UTF8.GetString(recvBuff,0,recvBytes);
+                    Console.WriteLine($"[From Client] {recvData}");
+
+                    // Send
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to TheWeakest Server!");
+                    clientSocket.Send(sendBuff);
+
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());  
+            }
         }
     }
 }
